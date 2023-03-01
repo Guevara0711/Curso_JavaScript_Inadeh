5389dff63c69a43713487e562c85e5e41f1afbc3
Alejandro Guevara <guevara@Alejandros-MacBook-Pro.local>
2023-02-28 20:53:00 -0500

    Clase del dia 28 de febrero de 2023

---
+++ Entradas_JavaScript/funcion1.js
@@ -0,0 +1,56 @@
+function suma(){
+    var num 
+    var resultado = 131
+
+    num = prompt("¿Cuanto es la suma de 36 + 45 + 50?")
+    if (num>='A' || num=='.'){
+        alert("Ha escrito un caracter no válido. Solo se aceptan números")
+    }else if(num == resultado){
+        alert("La respuesta es correcta")
+    }else{
+        alert("La respuesta es incorrecta. La respuesta es: "+ resultado)
+    }
+}
+
+function resta(){
+    var num1
+    var resultado1 = 34
+
+    num1 = prompt("¿Cuanto es la resta de 50 - 16")
+    if (num1>='A' || num1=='.'){
+        alert("Ha escrito un caracter no válido. Solo se aceptan números")
+    }else if(num1 == resultado1){
+        alert("La respuesta es correcta")
+    }else{
+        alert("La respuesta es incorrecta. La respuesta es: "+ resultado1)
+    }
+}
+
+function producto(){
+    var num2
+    var resultado2 = 25
+
+    num2 = prompt("¿Cuanto es producto de 5 X 5")
+    if (num2>='A' || num2=='.'){
+        alert("Ha escrito un caracter no válido. Solo se aceptan números")
+    }else if(num2 == resultado2){
+        alert("La respuesta es correcta")
+    }else{
+        alert("La respuesta es incorrecta. La respuesta es: "+ resultado2)
+    }
+}
+
+function division(){
+    var num3
+    var resultado3 = 10
+
+    num3 = prompt("¿Cuanto es la division de 20 / 2?")
+    if (num3>='A' || num3=='.'){
+        alert("Ha escrito un caracter no válido. Solo se aceptan números")
+    }else if(num3 == resultado3){
+        alert("La respuesta es correcta")
+    }else{
+        alert("La respuesta es incorrecta. La respuesta es: "+ resultado3)
+    }
+
+}
